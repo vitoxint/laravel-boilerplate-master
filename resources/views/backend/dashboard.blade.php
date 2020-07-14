b3c4000f60e34f856b052104df51221ec2b242c3
Victor Monsalves <vitoxint@gmail.com>
2020-07-14 12:42:42 -0400

    chasrt 3

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -10,16 +10,23 @@
                     <strong>@lang('strings.backend.dashboard.welcome') {{ $logged_in_user->name }}!</strong>
                 </div><!--card-header-->
                 <div class="card-body">
-                    {!! __('strings.backend.welcome') !!}
+                   <!--  {!! __('strings.backend.welcome') !!} -->
  
-<!--                     <div id="app">
+                    <div id="app">
                         <div class="row mb-4">
                             <div class="col">
-                                <example-component2></example-component2>
+                                <!-- <example-component2></example-component2> -->
+                                <grafica-component></grafica-component>
                             </div> 
                         </div> 
 
-                    </div> -->
+                    </div>
+
+                    <div class="row">
+                        <div class="col col-md-5">
+                                <grafica-component></grafica-component>
+                        </div>
+                    </row>
 
 
                 </div><!--card-body-->
