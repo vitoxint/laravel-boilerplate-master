048199e31d27fa7e264dc14dc2dbf5a92d1fabe0
Victor Monsalves <vitoxint@gmail.com>
2020-08-17 13:15:09 -0400

    fix etapa item ot +  index completo etapas 2

--- resources/views/backend/etapa_itemots/includes/header-buttons.blade.php
+++ resources/views/backend/etapa_itemots/includes/header-buttons.blade.php
@@ -1,4 +1,6 @@
 <div class="btn-toolbar float-right" role="toolbar" aria-label="@lang('labels.general.toolbar_btn_groups')">
+
+    <a href="{{ route('admin.etapa_itemots.create',$item_ot ,$trabajo) }}" class="btn btn-success ml-1" data-toggle="tooltip" title="@lang('labels.general.create_new')"><i class="fas fa-plus-circle"></i></a>
  
     @can('administrar ordenes de trabajo')
     <a href="{{ route('admin.etapa_itemots.exportarxls') }}" class="btn  ml-1" style="background-color:#006400; color:white;" data-toggle="tooltip" title="Exportar Excel"><i class="fas fa-file-excel"></i> Exportar</a>
